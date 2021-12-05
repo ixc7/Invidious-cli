0597f9f725f4fac5cf1702e86d29ac71a31524d4
Your name here <isaacxpreston7@gmail.com>
2021-12-05 04:06:26 -0500

    search works

--- search.js
+++ search.js
@@ -1,5 +1,9 @@
 import https from 'https'
 
+
+
+// https://api.invidious.io/instances.json?pretty=1&sort_by=type,users
+
 const hosts = [
   'vid.puffyan.us', // []
   'invidious.osi.kr', // []
@@ -18,20 +22,22 @@ const hosts = [
   'invidio.xamh.de', // correct
 ]
 
-const num = (hosts.length - 1)
+const hostname = hosts[(hosts.length - 1)]
 const query = 'foo bar'
 const page = '1'
 
 const search = () => {
   return new Promise((resolve, reject) => {
 
+
     const query = new URL(
       '/api/v1/search?q=awesome', 
-      `https://${hosts[num]}/api`
+      `https://${hostname}/api`
     )
 
     query.searchParams.set('q', query)
     query.searchParams.set('page', page)
+    query.searchParams.set('pretty', 1)
 
     const req = https.request(query.href)
 
@@ -43,25 +49,35 @@ const search = () => {
       })
 
       res.on('end', () => {
-        // resolve(`results:\n\n${resToString}`)
         resolve(resToString)
       })
     })
 
     req.end()
-  })
 
+    
+  })
 }
 
 async function init () {
   const res = await search()
-  console.log(`server: ${hosts[num]}\nsearch: ${query}\nresults: ${JSON.parse(res).length}\npage: ${page}\n`)
-  console.log(JSON.parse(res, 0, 2).map(item => {
-    return {
-      title: item.title,
-      id: item.videoId
-    }
-  }))
+  const resParsed = JSON.parse(res)
+  const resMapped = resParsed.map(item => {
+        return {
+          title: item.title,
+          id: item.videoId
+        }
+      })
+
+  console.log('\x1b[?25h\x1b[0m\x1Bc\x1b[3J')
+  
+  console.log(`
+    \rserver: ${hostname}
+    \rsearch: ${query}
+    \rresults: ${resParsed.length}
+    \rpage: ${page}\n
+  `)
+  console.log(resMapped)
   console.log('\n')
 }
 
