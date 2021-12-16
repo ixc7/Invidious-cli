3067a0845e8ef078380e51ae3a73be15cbb39589
Your name here <isaacxpreston7@gmail.com>
2021-12-16 10:05:44 -0500

    readline function

--- search.js
+++ search.js
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import https from 'https'
 
 const hosts = [
@@ -12,21 +13,23 @@ const hosts = [
   'yt.didw.to', // 502: bad gateway
   'invidious.kavin.rocks', // 1020
   'invidious-us.kavin.rocks', // 1020
-  'invidious-us.kavin.rocks', // 1020
   'vid.mint.lgbt', // correct
   'inv.riverside.rocks', // correct
   'invidio.xamh.de', // correct
 ]
-// TODO check hosts
 
-// let hostURL = 4
-let serverIndex = 4
-let serverMax = hosts.length
+const leave = (x) => {
+  console.log(x)
+  process.exit(0)
+}
+
+const userInput = 'humanity is the devil'
+
+let serverIndex = 1
 let server = hosts[(hosts.length - serverIndex)]
+const serverMax = hosts.length
+const maxpages = 10
 const client = 'yewtu.be'
-
-const searchterm = 'hello world'
-const maxpages = 100
 
 const search = (p) => {
   return new Promise((resolve, reject) => {
@@ -35,14 +38,13 @@ const search = (p) => {
       `https://${server}/api`
     )
 
-    query.searchParams.set('q', searchterm)
+    query.searchParams.set('q', userInput)
     query.searchParams.set('page', p)
     query.searchParams.set('pretty', 1)
 
     const req = https.request(query.href)
 
     req.on('response', res => {
-
       let resToString  = ''
 
       res.on('data', chunk => {
@@ -51,19 +53,16 @@ const search = (p) => {
 
       res.on('end', () => {
         if (res.statusCode !== 200) {
-            console.log('BAD STATUS CODE:', res.statusCode)
-            if (serverIndex >= serverMax) {
-              console.log('all servers are down.')
-              reject('no servers')
-            } else {
-              console.log(`server '${server}' is down. trying next...`)
-              serverIndex -= 1
-              server = hosts[(hosts.length - serverIndex)]
-              resolve(search (p))
-            }
-        } 
-        // console.log('GOT RES:', resToString)
-        console.log('GOT STATUS CODE:', res.statusCode)
+          console.log(`server '${server}' is down (${res.statusCode}).`)
+          if (serverIndex >= serverMax) {
+            leave('all servers are down.')
+          } else {
+            serverIndex += 1
+            server = hosts[(hosts.length - serverIndex)]
+            console.log(`trying '${server}'`)
+            resolve(search (p))
+          }
+        }
         resolve(resToString)
       })
     })
@@ -73,14 +72,11 @@ const search = (p) => {
 }
 
 
-const leave = (input) => {
-  console.log(input)
-  process.exit(0)
-}
 
 (async () => {
   let final = {}
-  console.log('\x1b[?25h\x1b[0m\x1Bc\x1b[3J')
+  console.log('\x1b[?25h\x1b[0m\x1Bc\x1b[3J\x1b[H')
+  console.log(`server: ${server}\nmax pages: ${maxpages}\n`)
 
   for (let i = 1; i < (maxpages + 1); i += 1) {
     console.log(`fetching page ${i} of ${maxpages}`)
@@ -101,6 +97,6 @@ const leave = (input) => {
 
     final[i] = resMapped
   }
-  // console.log(final)
+  fs.writeFileSync('results.json', JSON.stringify(final, 0, 2))
   leave(final)
 })()
