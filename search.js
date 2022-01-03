fe1d9971c07f358e737bb22310872aa25d8f65bd
Your name here <isaacxpreston7@gmail.com>
2022-01-03 11:29:39 -0500

    multiple readline interfaces select.js (for pagination later.)

--- search.js
+++ search.js
@@ -1,129 +1,92 @@
 import https from 'https'
 import getInstances from './instances.js'
 
-// env vars
-let MAXPAGES = 1
+let MAXPAGES = 2
 let USERINPUT = 'hello cruel world'
-// if ((process.argv.slice(2)).length) USERINPUT = process.argv.slice(2).join(' ')
 
-// trap <command> EXIT
-// TODO can we make this default behaviour?
-// const leave = (i) => {
-  // return i
-  // process.stdout.write('\n')
-  // if (i) console.log(i)
-  // return i
-  // process.exit(0)
-// }
 
-// init wraps:
-// - search: requests 1 page of results
-// - searchRecursive: calls search() recursively
-// const init = async (userInput = USERINPUT, maxpages = MAXPAGES) => {
-  const loadEnv = async () => {
-    const hosts = await getInstances()  
-    const serverMax = hosts.length
-    let serverIndex = 1
-    let server = hosts[(serverMax - serverIndex)]
-
-    return {
-      hosts,
-      serverMax,
-      serverIndex,
-      server,
-    }
+// TODO refactor this so we're not calling it for EVERY PAGE.
+const loadEnv = async () => {
+  const hosts = await getInstances()    
+  return {
+    hosts,
+    serverMax: hosts.length,
+    serverIndex: 1,
+    server: hosts[(hosts.length - 1)],
   }
+}
 
+// request 1 page
+const search = async (p) => {
 
-  // request 1 page
-  const search = async (p) => {
+  let env = await loadEnv()
+  let { hosts, serverMax, serverIndex, server } = env
+  
+  return new Promise((resolve, reject) => {
+    const query = new URL(
+      `/api/v1/search`, 
+      `${server}/api`
+    )
 
-    let env = await loadEnv()
-    let { hosts, serverMax, serverIndex, server } = env
-    
-    return new Promise((resolve, reject) => {
-      const query = new URL(
-        `/api/v1/search`, 
-        `${server}/api`
-      )
+    query.searchParams.set('q', USERINPUT)
+    query.searchParams.set('page', p)
+    query.searchParams.set('pretty', 1)
 
-      // query.searchParams.set('q', userInput)
-      query.searchParams.set('q', USERINPUT)
-      query.searchParams.set('page', p)
-      query.searchParams.set('pretty', 1)
+    const req = https.request(query.href)
 
-      const req = https.request(query.href)
+    req.on('response', res => {
+      let resToString  = ''
 
-      req.on('response', res => {
-        let resToString  = ''
+      res.on('data', chunk => resToString += chunk.toString('utf8'))
 
-        res.on('data', chunk => resToString += chunk.toString('utf8'))
-
-        res.on('end', () => {
-          // try different server if current is down
-          if (res.statusCode !== 200) {
-            console.log(`server '${server}' is down (${res.statusCode}).`)
-          
-            // exit if all servers have been tried.
-            if (serverIndex >= serverMax) {
-              // leave('all servers are down.')
-              return false
-            } else {
-              serverIndex += 1
-              server = hosts[(hosts.length - serverIndex)]
-              // console.log(`trying '${server}'`)
-
-              // keep trying servers recursively, and resolve the first successful result.
-              resolve(search (p))
-            }
+      res.on('end', () => {
+        // try different server if current is down
+        if (res.statusCode !== 200) {
+          console.log(`server '${server}' is down (${res.statusCode}).`)
+        
+          // return false if all servers have been tried.
+          if (serverIndex >= serverMax) {
+            return false
+          // keep trying servers recursively, and return the first successful result.
+          } else {
+            serverIndex += 1
+            server = hosts[(hosts.length - serverIndex)]
+            resolve(search(p))
           }
+        }
 
-          // resolve the first result if it didn't return an error.
-          resolve(resToString)
-        })
+        // return result if first server is up.
+        resolve(resToString)
+      })
+    })
+
+    req.end()
+  })
+}
+
+// request (1-MAXPAGES) number of pages
+// break and return if MAXPAGES is reached, or no more results are found.
+const searchRecursive = async (userInput = USERINPUT, maxpages = MAXPAGES) => {
+    let final = {}
+
+    for (let i = 1; i < (maxpages + 1); i += 1) {
+
+      const res = await search(i)
+      if (!res) return false
+      
+      const resJSON = JSON.parse(res)
+      if (resJSON.length < 1) return final
+      
+      const resMapped = resJSON.map(item => {
+        return {
+          name: item.title,
+          value: item.videoId
+        }
       })
 
-      req.end()
-    })
-  }
+      final[i] = resMapped
+    }
+    return final
+}
 
-  // request (1-MAXPAGES) number of pages
-  // break and return if MAXPAGES is reached, or no more results are found.
-  const searchRecursive = async (userInput = USERINPUT, maxpages = MAXPAGES) => {
-      let final = {}
-      // process.stdout.write(`\x1b[?25h\x1b[0m\x1Bc\x1b[3J\x1b[Hinput: ${userInput}\nserver: ${server}\nmax pages: ${maxpages}\n`)
-
-      for (let i = 1; i < (maxpages + 1); i += 1) {
-        // process.stdout.write(`\rfetching page ${i} of ${maxpages}\r`)
-
-        const res = await search(i)
-        if (!res) return false
-        // if (!res) cleanup()
-        
-        const resJSON = JSON.parse(res)
-        if (resJSON.length < 1) return final
-        // if (resJSON.length < 1) leave(final)
-        
-        const resMapped = resJSON.map(item => {
-          return {
-            title: item.title,
-            url: item.videoId
-            // url: `${server}/watch?v=${item.videoId}`
-          }
-        })
-
-        final[i] = resMapped
-      }
-      return final
-      // leave(final)
-  }
-
-  // ok let's go fingers crossed
-  // searchRecursive()
-// }
-
-// init()
-// export default init
-
-// console.log((await searchRecursive())[1])
 export { loadEnv, search, searchRecursive }
--- select.js
+++ select.js
@@ -1,3 +1,4 @@
+import readline from 'readline'
 import pkg from 'enquirer'
 const { AutoComplete } = pkg
 import { loadEnv, search, searchRecursive } from './search.js'
@@ -8,6 +9,17 @@ const prompt = new AutoComplete({
   name: 'flavor',
   message: 'Pick your favorite flavor',
   choices: results
+})
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+process.stdin.on('keypress', (char, props) => {
+  // rl.cursorTo(0, 0)
+  readline.cursorTo(process.stdout, 50, 0)
+  console.log('keypress', char, char, char)
 })
 
 prompt.run()
