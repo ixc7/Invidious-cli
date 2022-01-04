4d9531fcb14dca07883b073dfedb50d8c12c3d3c
Your name here <isaacxpreston7@gmail.com>
2022-01-04 03:41:49 -0500

    cleaup index.js

--- index.js
+++ index.js
@@ -12,66 +12,41 @@ if (!process.argv[2]) {
   process.exit(0)
 }
 
-let searchTerm = process.argv.slice(2).join(' ')
-
-console.clear()
-console.log(`searching for: ${searchTerm}`)
-
-const results = await searchRecursive(searchTerm, MAX_PAGES)
-// let choices = ['CANCEL']
-let choices = []
-
-for (let key in results) {
-  choices = choices.concat(results[key])
-}
-
-
-let acceptKeypress = false
+const searchTerm = process.argv.slice(2).join(' ')
 
 const prompt = new AutoComplete({
   name: 'video',
   message: 'select a video',
-  choices,
-  limit: (process.stdout.rows - 4),
-  validate: () => {
-    return acceptKeypress === true
-  }
+  choices: async () => {
+    const results = await searchRecursive(searchTerm, MAX_PAGES)
+    let choices = []
+    for (let key in results) {
+      choices = choices.concat(results[key])
+    }
+    return choices
+  },
+  limit: (process.stdout.rows - 4)
 })
 
 prompt.on('keypress', (char, props) => {
-  // console.log(props.name)
   readline.cursorTo(process.stdout, 0, process.stdout.rows - 2)
   readline.clearLine(process.stdout, 0)
-  console.log('key pressed:', props.name || 'none')
-  if (props.name !== 'return') acceptKeypress = true
+  if (props.name === 'left' || props.name === 'right') console.log(props.name)
   readline.cursorTo(process.stdout, 0, 0)
 })
 
-/*
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-rl.input.on('keypress', (char, props) => {
-  readline.cursorTo(rl.output, 0, rl.output.rows - 2)
-  readline.clearLine(rl.output, 0)
-  console.log('key pressed:', props.name || 'none')
-  readline.cursorTo(rl.output, 0, 0)
-})
-*/
-
 try {
   console.clear()
+  console.log(`searching for: ${searchTerm}`)
   const selection = await prompt.run()
   console.clear()
   console.log(`opening url with ${VIDEO_PLAYER}: ${selection}`)
-  // const videoPlayer = spawn(
-    // VIDEO_PLAYER,
-    // [selection, '--fullscreen', '--loop', '--audio-pitch-correction=no'],
-    // { detached: true, stdio: 'ignore' }
-  // )
-  // videoPlayer.unref()
+  const videoPlayer = spawn(
+    VIDEO_PLAYER,
+    [selection, '--fullscreen', '--loop', '--audio-pitch-correction=no'],
+    { detached: true, stdio: 'ignore' }
+  )
+  videoPlayer.unref()
   process.exit(0)
 }
 
