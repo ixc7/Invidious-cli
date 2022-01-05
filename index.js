182f4af55777be0a5182aeac351e28a45f8b9cab
Your name here <isaacxpreston7@gmail.com>
2022-01-05 15:59:03 -0500

    search, filter and select results with up/down arrow keys

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ const searchTerm = process.argv.slice(2).join(' ')
 const VIDEO_PLAYER = 'mpv'
 const MAX_PAGES = 1
 
-console.clear()
 console.log(`searching for ${searchTerm}`)
 const searchResults = await searchRecursive(searchTerm, MAX_PAGES)
-console.clear()
 console.log('got results')
 
 const rl = readline.createInterface({
@@ -28,29 +26,46 @@ const fzf = new Fzf(searchResults, {
 })
 
 let input = ''
+let position = 0
 let selection = false
+let matches = []
 
 process.stdin.on('keypress', (char, props) => {
-  console.clear()
-  readline.cursorTo(process.stdout, 1, 0)
+  // console.clear()
 
   // AND HERE WE HAVE TO CAPTURE ARROWS AND ENTER KEY AS WELL
+  // ENTER KEY OPENS MPV
+  // PROBABLY WANT A BACK/REDO BUTTON TOO
   if (props.name === 'backspace') {
     input = input.substring(0, input.length - 1)
-  } else if (char){
-    input = input.concat(char)
   }
-  
-  const matches = fzf.find(input).map(match => match.item.name)
-  // SO HERE WE HAVE TO COUNT ARROWS AND NOT CLEAR THE CONSOLE IF ITS ARROWS AND JUST UPDATE THE SELECTION.
-  selection = matches[0]
+  else if (props.name === 'return') {
+    if (selection) console.log(`result: ${selection}`)
+    process.exit(0)
+  } 
+  else if (props.name === 'down' && matches[position + 1]) {
+    position += 1
+    selection = matches[position]
+  }
+  else if (props.name === 'up' && matches[position - 1]) {
+    position -= 1
+    selection = matches[position]
+  }
+  else if (char) {
+    input = input.concat(char)
+    selection = matches[0]
+  }
 
-  // THIS IS WHAT THE USER SEES.
-  readline.cursorTo(process.stdout, 0, 0)
-  readline.clearLine(process.stdout, 0)
-  console.log(input)
-  console.log(matches)
-  readline.cursorTo(process.stdout, input.length, 0)
+  matches = fzf.find(input).map(match => match.item.name)
+
+  // SO HERE WE HAVE TO COUNT ARROWS AND NOT CLEAR THE CONSOLE IF ITS ARROWS AND JUST UPDATE THE SELECTION.
+
+  console.log('\x1b[0m\x1Bc\x1b[3J\x1b[?25l')
+  if (matches[0]) console.log(matches.join('\n'))
+  readline.cursorTo(process.stdout, 0, process.stdout.rows - 4)
+  console.log(`input: ${input}`)
+  console.log(`selection: ${selection}`)
+  console.log('\x1b[?25h')
 })
 
 // try {
