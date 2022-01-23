76637f3a4256bd2aaf334fcb8aafa651650ffca8
ixc7 <isaacxpreston7@gmail.com>
2022-01-23 06:07:29 -0500

    video options

--- options.js
+++ options.js
@@ -1,4 +1,5 @@
-export default {
+
+const musicPlayer = {
   downloader: 'yt-dlp', // || youtube-dl
   format: 'm4a', // yt-dlp --list-formats <url>, m4a = audio, mp4 = video
   player: 'mpv', // || anything else that takes the filename/path as the first argument
@@ -9,3 +10,20 @@ export default {
   pages: 3, // max pages to fetch
   repeat: true // repeat search the prompt forever
 }
+
+const videoPlayer = {
+  downloader: 'yt-dlp', // || youtube-dl
+  format: 'mp4', // yt-dlp --list-formats <url>, m4a = audio, mp4 = video
+  player: 'mpv', // || anything else that takes the filename/path as the first argument
+  playerOptions: [ // || anything else
+    '--audio-pitch-correction=no',
+    '--autofit=25%',
+    '--geometry=100%:100%',
+    '--ontop',
+    '--loop',
+  ],
+  pages: 3, // max pages to fetch
+  repeat: true // repeat search the prompt forever
+}
+
+export default musicPlayer
