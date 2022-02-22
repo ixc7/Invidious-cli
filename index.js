021488549b1e97aa4c19206982502dbf70efc0f6
ixc7 <isaacxpreston7@gmail.com>
2022-02-21 23:04:28 -0500

    cleanup keypress.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ import config from './config.js'
 const run = async () => {
   const dir = config.save ? config.folder : mktemp()
   const results = await search()
-  const matches = results.map(m => m.title)
+  // const matches = results.map(m => m.title)
   const rl = mkInterface()
-  const handler = await mkParser(matches, results, dir, rl)
+  const handler = await mkParser(results, dir, rl)
 
   rl.input.on('keypress', handler)
 }
--- keypress.js
+++ keypress.js
@@ -3,12 +3,12 @@ import { Fzf } from 'fzf'
 import downloadFile from './download.js'
 import { bold, formatTime, noScroll } from './util.js'
 
-const mkParser = async (matchList, searchResultsList, destinationFolder, rl) => {
+const mkParser = async (searchResultsList, destinationFolder) => {
   const fzf = new Fzf(searchResultsList, { selector: item => item.title })
-  let input = ''
+  let selection = false
   let render = false
   let position = 0
-  let selection = matchList[0]
+  let input = ''
 
   const parser = async (char, props) => {
     // -- SUBMIT
@@ -23,49 +23,44 @@ const mkParser = async (matchList, searchResultsList, destinationFolder, rl) => 
     if (props.name === 'backspace') input = input.substring(0, input.length - 1)
     else if (props.name === 'down') position += 1
     else if (props.name === 'up') position -= 1
-    else if (char && !props.sequence.includes('\x1b')) input += char 
+    else if (char && !props.sequence.includes('\x1b')) input += char
 
     // ---- RENDERER
+    // TODO thumbnails
     if (render) {
-      noScroll()
       render = false
-
-      const matchingItems = fzf.find(input).map(({item}) => {
+            
+      const matchingItems = fzf.find(input).map(({item}, index) => {
         return {
           title: item.title,
           info: item.info,
-          url: item.url
+          url: item.url,
+          index
         }
       })
 
-      if (position > matchingItems.length - 1) {
-        position = 0
-        selection = matchingItems[0]
-      } else if (position < 0) {
-        position = matchingItems.length - 1
-        selection = matchingItems[matchingItems.length - 1]
-      } else selection = matchingItems[position]
+      if (position > matchingItems.length - 1) position = 0
+      else if (position < 0) position = matchingItems.length - 1
 
-      // TODO thumbnails
-      if (matchingItems[0]) {
-        const display = matchingItems.map(
-          (item, index) => {
-            if (index === position) return bold(item.title)
-            return item.title
-          }
-        )
-        .slice(position)
-        .slice(0, process.stdout.rows - 9)
-        .join('\n')
+      selection = matchingItems[position]
 
+      noScroll()
+
+      if (selection) {
+        const { author, viewCount, publishedText, lengthSeconds } = selection.info
+        
         cursorTo(process.stdout, 0, 0)
-        process.stdout.write(display)
-      }
+        process.stdout.write(
+          `${bold(selection.title)}\n` +
+          matchingItems
+          .slice(position + 1, position + (process.stdout.rows - 9))
+          .map(res => res.title)
+          .join('\n')
+        )
 
-      if (matchingItems[position]) { 
         cursorTo(process.stdout, 0, process.stdout.rows - 7)
-        const { author, viewCount, publishedText, lengthSeconds } = matchingItems[position].info
-        process.stdout.write(`
+        process.stdout.write(
+        `
           \rselection: ${selection.title}
           \rauthor: ${author}
           \rviewCount: ${viewCount}
@@ -83,12 +78,11 @@ const mkParser = async (matchList, searchResultsList, destinationFolder, rl) => 
   // TODO duplicate
   noScroll()
   cursorTo(process.stdout, 0, 0)
-  process.stdout.write(searchResultsList
+  process.stdout.write(
+    // `${bold(searchResultsList[0].title)}\n` +
+    searchResultsList
     .slice(0, process.stdout.rows - 9)
-    .map((item, index) => {
-      if (index === 0) return bold(item.title)
-      return item.title
-    })
+    .map(res => res.title)
     .join('\n')
   )
 
